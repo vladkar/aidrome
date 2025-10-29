c058c67104ab78c71f9dde596fe4c4d10908feb0
vladkar <vladislav.k.work@gmail.com>
2025-10-29 23:29:45 +0100

    update loader

--- src/loader.js
+++ src/loader.js
@@ -1,4 +1,5 @@
 console.log("loader.js injected");
 const s = document.createElement("script");
 s.src = "custom/custom-init.js";
+s.type = "module"; // Enable ES6 module support
 document.head.appendChild(s);
